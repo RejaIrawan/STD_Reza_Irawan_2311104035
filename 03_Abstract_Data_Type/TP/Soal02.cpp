ed868a98a0c4652c3998132465407d669810e9c7
Rezairawan <08reza.irawan@gmail.com>
2024-10-13 22:33:44 +0700

    Add files via upload

---
+++ 03_Abstract_Data_Type/TP/Soal02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+// agar tahu alamat suatu Variabel
+
+int main(){
+    int a = 12;
+
+    cout << "Alamat dari Var A: " << &a << endl;
+    
+    return 0;
+}
